846a33e4313c37550b8c888f4102aa6c30babfb4
Sadaq Ahmed <z3ro770@gmail.com>
2026-01-01 07:06:46 -0800

    chore: remove capacitor config from web build

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.linkup.app',
-  appName: 'LinkUp',
-  webDir: 'out',
-};
-
-export default config;
